0dc8882b6fdb657e35e89cf4f0dad4e3461350df
FarokhitdinArtikgaliev <mushtatie@gmail.com>
2026-09-07 19:15:10 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,8 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "uz.kadastr.route",
-  appName: "Кадастр Маршрут",
-  webDir: "www",
-  server: { androidScheme: "https" },
-};
-
-export default config;
+{
+  "appId": "uz.kadastr.route",
+  "appName": "Кадастр Маршрут",
+  "webDir": "www",
+  "server": {
+    "androidScheme": "https"
+  }
+}
